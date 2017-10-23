8e965be5e88db11f96a9a49e397d57bcf1776c2d
karan <kirubakarann7@gmail.com>
2017-10-23 09:56:22 +0530

    Scaffolds added.

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+      omniauth_callbacks: 'users/omniauth_callbacks',
+  }
+  resources :appointments
+  resources :patients
+  resources :doctors
+  root to: "appointments#index"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
